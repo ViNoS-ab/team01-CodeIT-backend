677e178b9ab07be41669df235e8e2d183084857d
Soapiane <110976108+Soapiane@users.noreply.github.com>
2023-02-09 23:41:07 +0100

    Update settings.py

--- organizers_app/organizers_app/settings.py
+++ organizers_app/organizers_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vbw@vran)@^9-h(s#)t$c%*e-l)4$sjg_9op0qplaqz8(p&2mp'
+SECRET_KEY = 'SOME_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
